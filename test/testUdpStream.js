b513e2e694fe4fc4c46e62207cba10811634a460
Dimitri del Marmol <dimitri.delmarmol@gmail.com>
2015-04-08 00:01:31 +0200

    bunyan test

--- test/testUdpStream.js
+++ test/testUdpStream.js
@@ -5,6 +5,7 @@ var expect = chai.expect;
 var dgram = require('dgram');
 var fs = require('fs');
 var path = require('path');
+var bunyan = require('bunyan');
 
 var SimpleUdpStream = require('../index');
 
@@ -52,6 +53,37 @@ describe("UDP stream", function () {
     receiver.bind(params.port, params.destination);
 
     fs.createReadStream(path.resolve(__dirname, 'test.message')).pipe(stream);
+  });
+
+  it("should receive a bunyan log message", function (done) {
+
+    var params = {
+      destination: '127.0.0.1',
+      port: 9999
+    };
+
+    var logger = bunyan.createLogger({
+      name: 'my-logger',
+      streams: [{
+        level: 'info',
+        stream: new SimpleUdpStream(params)
+      }]
+    });
+
+    var receiver = dgram.createSocket('udp4');
+    receiver.on('message', function (msg) {
+      var log = JSON.parse(msg.toString());
+      expect(log).to.have.all.keys(["value", "msg", "name", "hostname", "pid", "level", "time", "v"]);
+      expect(log.value).to.equal(1);
+      expect(log.msg).to.equal("Hello");
+
+      receiver.close();
+      done();
+    });
+    receiver.bind(params.port, params.destination);
+
+  logger.info({ value: 1 }, "Hello");
+
   });
 
 });
